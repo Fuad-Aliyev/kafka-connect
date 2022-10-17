0e588b65e4bae68a049191ee271e12d4b1dc064d
Fuad Aliyev <fuad@Fuads-MacBook-Pro.local>
2022-10-17 23:13:50 +0400

    first commit

---
+++ src/main/java/com/course/kafka/broker/consumer/CdcMarketingListener.java
@@ -0,0 +1,35 @@
+package com.course.kafka.broker.consumer;
+
+import com.course.kafka.broker.message.CdcMessage;
+import com.course.kafka.broker.message.MarketingPromotionMessage;
+import com.course.kafka.broker.message.MarketingSalesMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CdcMarketingListener {
+    private static final Logger LOG = LoggerFactory.getLogger(CdcMarketingListener.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "t-cdc-marketing.public.mkt_promotions")
+    public void listenPromotion(String message) throws JsonProcessingException {
+        CdcMessage promotion = objectMapper.readValue(message,
+                new TypeReference<CdcMessage<MarketingPromotionMessage>>() {});
+        LOG.info("Marketing promotion message received : {}", promotion);
+    }
+
+    @KafkaListener(topics = "t-cdc-marketing.public.mkt_sales")
+    public void listenSales(String message) throws JsonProcessingException {
+        CdcMessage promotion = objectMapper.readValue(message,
+                new TypeReference<CdcMessage<MarketingSalesMessage>>() {});
+        LOG.info("Marketing sales message received : {}", promotion);
+    }
+}
---
+++ src/main/java/com/course/kafka/config/JsonConfig.java
@@ -0,0 +1,15 @@
+package com.course.kafka.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JsonConfig {
+    @Bean
+    ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        return objectMapper;
+    }
+}
